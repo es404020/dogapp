f36a2e5dbb60af43711a8918cfd65452001d6682
ajani eniola <eniolsolomon@gmail.com>
2019-10-19 22:11:29 +0100

    Flutter firebase image upload

--- lib/screen/add_dog.dart
+++ lib/screen/add_dog.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:dogapp/ui/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:dogapp/classes/main.dart';
 import 'package:provider/provider.dart';
 import 'package:dogapp/service/main.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 
 class AddPost extends StatefulWidget {
@@ -16,9 +22,14 @@ class _AddPostState extends State<AddPost> {
 
   final name = TextEditingController();
   final description = TextEditingController();
-  final dog_url  = TextEditingController();
+ String dog_url  = '';
   DogService dogservices = new DogService();
+ File _imageFile;
+ bool _loading= false;
 
+ final FirebaseStorage _storage = FirebaseStorage(storageBucket: 'gs://dogapp-a20f7.appspot.com/');
+ String Url;
+  StorageUploadTask _uploadTask;
 
   @override
   Widget build(BuildContext context) {
@@ -27,86 +38,142 @@ class _AddPostState extends State<AddPost> {
 
     
     return Scaffold(
+
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon:Icon(Icons.photo_camera),
+              onPressed: ()=>_pickImage(ImageSource.camera),
+            ),
+            IconButton(
+                icon:Icon(Icons.photo_library),
+              onPressed: ()=>_pickImage(ImageSource.gallery),
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text("Add DOG"),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
+      body:  ModalProgressHUD(
+        child: Center(
 
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-        TextField(
-          controller: name,
-        decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Dog Name',
-          enabledBorder: const OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.white, width: 0.0),
-          ),
-        ),
-      ),TextField(
-              controller:description,
-              maxLines: 4,
-      decoration: InputDecoration(
-
-          border: InputBorder.none,
-          hintText: 'Description',
-        enabledBorder: const OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.white, width: 0.0),
-        ),
-      ),
-    ),
-      TextField(
-        controller:dog_url,
-
-        decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'dog url',
-          enabledBorder: const OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.white, width: 0.0),
-          ),
-        ),
-      ),
-            const SizedBox(height: 30),
-            RaisedButton(
-              onPressed: () {
-             if(name.text == '' || description.text =='' || dog_url.text=='' ){
-
-AlertManger(context, "Incomplete input");
-             }
-             else{
-         var dog =    Dog(dog: name.text,dog_ur: dog_url.text,description: description.text,postid: '',email: user?.email);
-           dogservices.addDog(dog).then((res)=>{
-
-             AlertManger(context, "Dog was added"),
-             name.text='',description.text='',dog_url.text=''
-
-
-
-           }).catchError((err)=>{
-
-
-           AlertManger(context, "Something went wrong")
-
-
-           });
-
-
-             }
-
-              },
-              child: const Text(
-                  'Add DOG',
-                  style: TextStyle(fontSize: 20)
-              ),
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+          TextField(
+            controller: name,
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Dog Name',
+            enabledBorder: const OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.white, width: 0.0),
             ),
+          ),
+        ),TextField(
+                controller:description,
+                maxLines: 4,
+        decoration: InputDecoration(
 
-          ],
-        )
+            border: InputBorder.none,
+            hintText: 'Description',
+          enabledBorder: const OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.white, width: 0.0),
+          ),
+        ),
+    ),
+              if(_imageFile != null) ...[
+                Image.file(_imageFile,width: 100.0,height: 100,),
+
+                FlatButton(
+                  child: Icon(Icons.refresh),
+                  onPressed: _clear,
+                )
+              ],
+
+              const SizedBox(height: 30),
+              RaisedButton(
+                onPressed: () {
+                  _submit(name,description,user);
+
+                },
+                child: const Text(
+                    'Add DOG',
+                    style: TextStyle(fontSize: 20)
+                ),
+              ),
+
+            ],
+          )
+        ),
+        inAsyncCall: _loading,
       ),
     );
   }
 
+  Future<void> _pickImage(ImageSource source) async {
+    File selected = await ImagePicker.pickImage(source: source);
+    setState(() {
+      _imageFile = selected;
+    });
+  }
+
+  _clear(){
+    setState(() {
+      _imageFile = null;
+    });
+  }
+
+  Future _submit(name,desc,user){
+    _loading = true;
+
+    String filePath = '${DateTime.now()}.png';
+
+    setState(() {
+
+      _uploadTask = _storage.ref().child(filePath).putFile(_imageFile);
+
+
+      _uploadTask.onComplete.then((res) async =>{
+        Url = await res.ref.getDownloadURL(),
+        dog_url = Url.toString(),
+
+      if(name.text == '' || description.text =='' || dog_url=='' ){
+
+          AlertManger(context, "Incomplete input"),
+        _loading = false
+    }
+      else{
+
+      dogservices.addDog(Dog(dog: name.text,dog_ur: dog_url,description: desc.text,postid: '',email: user?.email)).then((res)=>{
+
+      AlertManger(context, "Dog was added"),
+      name.text='',description.text='',dog_url='',
+_loading = false,
+      _clear()
+
+
+
+      }).catchError((err)=>{
+
+_loading = false,
+      AlertManger(context, "Something went wrong")
+
+
+      })
+
+
+      }
+
+      });
+
+    });
+
+
+
+  }
 
 }
